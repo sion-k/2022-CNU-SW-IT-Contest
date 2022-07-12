7edbd034f0fedccb57291746f895699fa46bd055
sion-k <ygonepiece@naver.com>
2022-07-12 18:22:09 +0900

    checker bugfix

--- problem/typo/checker/checker.cpp
+++ problem/typo/checker/checker.cpp
@@ -1,11 +1,10 @@
 #include "testlib.h"
-#include <bits/stdc++.h>
 using namespace std;
  
 int main(int argc, char* argv[]) {
     registerTestlibCmd(argc, argv);
     int jury = ans.readInt();
-    int participant = ouf.readInt(1, 2147483647, "ans");
+    int participant = ouf.readInt(0, 2147483647, "ans");
     if (jury != participant) {
         quitf(_wa, "wa");
     }
