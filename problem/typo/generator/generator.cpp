559b077b1453fbab5a9c545815d892882798f3f0
sion-k <ygonepiece@naver.com>
2022-07-12 18:05:29 +0900

    generator added

---
+++ problem/typo/generator/generator.cpp
@@ -0,0 +1,9 @@
+#include "testlib.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+    cout << rnd.next("[\\(\\)]{1,100000}") << "\n";
+}
