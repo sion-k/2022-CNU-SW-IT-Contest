ffb0fde6af0c0d0cb9fedaf39d8aafd071cf0e96
sion-k <axiom0510@gmail.com>
2022-08-11 11:43:07 +0900

    feat: main validator added

---
+++ problem/hamburger/validator.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+ 
+using namespace std;
+ 
+int main(int argc, char* argv[]) {
+    registerValidation(argc, argv);
+    int a = inf.readInt(1, 100, "a");
+    inf.readSpace();
+    int b = inf.readInt(1, 100, "b");
+    inf.readEoln();
+    inf.readEof();
+}
