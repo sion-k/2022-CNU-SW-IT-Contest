fb53f103538bd18a90cd2dee544692f70278fd62
sion-k <axiom0510@gmail.com>
2022-08-11 10:57:57 +0900

    feat: template added

---
+++ template/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// 문제의 정답 코드입니다.
+int main() {
+
+}
---
+++ template/validator.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+ 
+using namespace std;
+ 
+int main(int argc, char* argv[]) {
+    registerValidation(argc, argv);
+    int n = inf.readInt(INT_MIN, INT_MAX, "n");
+    inf.readEoln();
+    inf.readEof();
+    ensuref(true, "This condition must be true");
+}
