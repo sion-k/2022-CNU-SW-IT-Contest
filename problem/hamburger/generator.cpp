1e31e50bb6fca7cf159fc38c7b3591b36ad6c7c4
sion-k <axiom0510@gmail.com>
2022-08-11 11:42:52 +0900

    feat: main generator added

---
+++ problem/hamburger/generator.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+    int a = rnd.next(1, 100);
+    int b = rnd.next(1, 100);
+    cout << a << " " << b << "\n";
+}
