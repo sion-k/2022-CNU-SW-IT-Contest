d0121f769973d772ca479b705448610b37921cfa
sion-k <axiom0510@gmail.com>
2022-08-15 18:11:36 +0900

    solution: pq naive solution added

---
+++ problem/all-day-pass-hard/solution/naive.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) {
+        cin >> x;
+    }
+    priority_queue<pair<int, int>> pq;
+    for (int i = 0; i < n; i++){
+        pq.emplace(a[i], i);
+    }
+    int prev = -1;
+    int sum = 0;
+    while (true) {
+        auto [v, i] = pq.top();
+        pq.pop();
+        if (v == 0) break;
+        if (prev == i) {
+            if (pq.empty()) break;
+            auto [nv, ni] = pq.top();
+            pq.pop();
+            if (nv == 0) break;
+            nv--;
+            prev = ni;
+            pq.emplace(nv, ni);
+        } else {
+            v--;
+            prev = i;
+        }
+        pq.emplace(v, i);
+        sum++;
+    }
+    cout << sum << "\n";
+}
