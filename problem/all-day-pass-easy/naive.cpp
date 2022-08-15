5169c051347028bde2cfa1792db6b45d106959e3
sion-k <axiom0510@gmail.com>
2022-08-15 15:16:34 +0900

    solution: naive solution optimization added

--- problem/all-day-pass-easy/naive.cpp
+++ problem/all-day-pass-easy/naive.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 
+#define ALL(x) (x).begin(), (x).end()
 #define FAST() cin.tie(0)->sync_with_stdio(0)
 
 using namespace std;
@@ -12,12 +13,16 @@ int main() {
     for (auto &x : a) {
         cin >> x;
     }
+    int max_a = *max_element(ALL(a));
     int prev = -1;
     int sum = 0;
     while (true) {
         pair<int, int> max;
         for (int i = 0; i < n; i++) if (i != prev) {
             max = ::max(max, {a[i], i});
+            if (max.first == max_a) {
+                break;
+            }
         }
         if (max.first == 0) break;
         a[max.second]--;
