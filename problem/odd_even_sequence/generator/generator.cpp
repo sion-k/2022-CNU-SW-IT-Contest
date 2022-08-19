9cfd8b6db19722cf6abd38600fc03166c5e6113b
sion-k <axiom0510@gmail.com>
2022-08-19 17:59:29 +0900

    generator: random generator added

---
+++ problem/odd_even_sequence/generator/generator.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+const int N_MIN = 1, N_MAX = 100;
+const int AI_MIN = 1, AI_MAX = 100;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+    int n = rnd.next(N_MIN, N_MAX);
+    cout << n << "\n";
+    for (int i = 0; i < n; i++) {
+        int ai = rnd.next(AI_MIN, AI_MAX);
+        cout << ai << " \n"[i == n - 1];
+    }
+}
