47e15045783b20f550f1ec72158ceaa572608f23
sion-k <axiom0510@gmail.com>
2022-08-20 12:59:37 +0900

    generator: almost all odd or all even generator

---
+++ problem/odd_even_sequence/generator/almost_odd_or_even.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+const int N_MIN = 1, N_MAX = 100;
+// 2를 곱해서 짝수를 만들거나, 2를 곱하고 1을 빼서 홀수로 만든다.
+const int AI_HALF_MIN = 1, AI_HALF_MAX = 50;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+    int n = rnd.next(N_MIN, N_MAX);
+    bool parity = rnd.next(0, 1);
+    cout << n << "\n";
+    for (int i = 0; i < n; i++) {
+        int ai = 2 * rnd.next(AI_HALF_MIN, AI_HALF_MAX) - parity;
+        // 4%의 확률로 다른 parity값을 만든다.
+        if (rnd.next(1, 100) <= 4) {
+            if (parity) {
+                ai++;
+            } else {
+                ai--;
+            }
+        }
+        cout << ai << " \n"[i == n - 1];
+    }
+}
