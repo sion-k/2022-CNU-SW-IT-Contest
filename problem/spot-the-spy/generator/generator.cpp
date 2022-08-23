f05ad6131f2065d41aed38e848c410da502e2679
sion-k <ygonepiece@naver.com>
2022-08-23 22:44:26 +0900

    fix: generator n range fixed

--- problem/spot-the-spy/generator/generator.cpp
+++ problem/spot-the-spy/generator/generator.cpp
@@ -7,13 +7,16 @@ using namespace std;
 // N의 범위
 const int N_MIN = 2, N_MAX = 100'000;
 
-// N * M 의 범위이자 a_i 의 범위
-const int NM_MIN = 2, NM_MAX = 1'000'000'000;
+// N * M의 범위
+const int NM_MIN = 4, NM_MAX = 1'000'000'000;
+
+// a의 범위
+const int AI_MIN = 2, AI_MAX = 1'000'000'000;
 
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
     int nm = rnd.next(NM_MIN, NM_MAX);
-    int n = rnd.next(N_MIN, N_MAX);
+    int n = rnd.next(max(N_MIN, nm / N_MAX), min(N_MAX, nm / 2));
     int m = rnd.next(2, nm / n);
     int k = m * rnd.next(1, n) - rnd.next(1, m - 1);
     if (k == 1) {
