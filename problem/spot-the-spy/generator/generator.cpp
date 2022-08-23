2af0a39b3837ea8f428e41a4f927b4b73599f8c8
sion-k <ygonepiece@naver.com>
2022-08-23 21:07:38 +0900

    generator: random generator

--- problem/spot-the-spy/generator/generator.cpp
+++ problem/spot-the-spy/generator/generator.cpp
@@ -4,13 +4,18 @@
 #define ALL(x) (x).begin(), (x).end()
 using namespace std;
 
-const int NM_MIN = 2, NM_MAX = 100;
+// N의 범위
+const int N_MIN = 2, N_MAX = 100'000;
+
+// N * M 의 범위이자 a_i 의 범위
+const int NM_MIN = 2, NM_MAX = 1'000'000'000;
 
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
-    int n = rnd.next(NM_MIN, NM_MAX);
-    int m = rnd.next(NM_MIN, NM_MAX);
-    int k = m * rnd.next(1, NM_MAX) - rnd.next(1, m - 1);
+    int nm = rnd.next(NM_MIN, NM_MAX);
+    int n = rnd.next(N_MIN, N_MAX);
+    int m = rnd.next(2, nm / n);
+    int k = m * rnd.next(1, n) - rnd.next(1, m - 1);
     if (k == 1) {
         k = m == 2 ? 3 : 2;
     }
