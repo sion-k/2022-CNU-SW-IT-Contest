1be216bc1c99ed57b9139bbe4faf518f70131a76
sion-k <ygonepiece@naver.com>
2022-08-23 23:23:33 +0900

    generator: random generator

--- problem/tree_and_query/generator/generator.cpp
+++ problem/tree_and_query/generator/generator.cpp
@@ -13,6 +13,11 @@ int main(int argc, char* argv[]) {
     
     cout << n << "\n";
     
+    for (int i = 0; i < n; i++) {
+        int ai = rnd.next(0, 1);
+        cout << ai << " \n"[i == n - 1];
+    }
+    
     vector<int> p(n);
     /* setup parents for vertices 1..n-1 */
     for(int i = 1; i <= n - 1; i++) {
