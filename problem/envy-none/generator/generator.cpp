b43e2e088bf01655ce94ab6e09fb80dc6b888ffa
sion-k <axiom0510@gmail.com>
2022-08-29 13:43:54 +0900

    feat: generator

---
+++ problem/envy-none/generator/generator.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+const int N_MIN = 1, N_MAX = 200'000;
+const long long AI_MIN = 1, AI_MAX = 1'000'000'000'000LL;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+    int n = rnd.next(N_MIN, N_MAX);
+    cout << n << "\n";
+    for (int i = 0; i < n; i++) {
+        long long ai = rnd.next(AI_MIN, AI_MAX);
+        cout << ai << " \n"[i == n - 1];
+    }
+}
