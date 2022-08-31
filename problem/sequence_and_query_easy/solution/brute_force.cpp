6134e673e8edbb09656b5b8edf089f0e9abe6b75
sion-k <axiom0510@gmail.com>
2022-08-31 15:39:52 +0900

    fix: easy version rename

---
+++ problem/sequence_and_query_easy/solution/brute_force.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define FAST() cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+
+int query(int n, vector<int>& a, int l, int r) {
+    int max = 0;
+    for (int i = l; i <= r; i++) {
+        for (int j = i; j <= r; j++) {
+            max = ::max(max, a[j] - a[i]);
+        }
+    }
+    return max;
+}
+
+int main() {
+    FAST();
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) {
+        cin >> x;
+    }
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int o;
+        cin >> o;
+        if (o == 1) {
+            int k, v;
+            cin >> k >> v;
+            a[k - 1] = v;
+        } else {
+            int l, r;
+            cin >> l >> r;
+            cout << query(n, a, l - 1, r - 1) << "\n";
+        }
+    }
+}
