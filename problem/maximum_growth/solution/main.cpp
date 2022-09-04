f44f46687eba10349fd2deff9ffcc5cfe1519549
sion-k <ygonepiece@naver.com>
2022-09-04 18:59:17 +0900

    fix: main solution

--- problem/maximum_growth/solution/main.cpp
+++ problem/maximum_growth/solution/main.cpp
@@ -2,19 +2,6 @@
 
 using namespace std;
 
-int query(int n, vector<int>& a, int l, int r) {
-    int max = 0;
-    vector<int> dp(n);
-    for (int i = l; i <= r; i++) {
-        dp[i] = a[i];
-        if (i - 1 >= l) {
-            dp[i] = min(dp[i], dp[i - 1]);
-        }
-        max = ::max(max, a[i] - dp[i]);
-    }
-    return max;
-}
-
 int main() {
     int n;
     cin >> n;
@@ -22,19 +9,10 @@ int main() {
     for (auto &x : a) {
         cin >> x;
     }
-    int q;
-    cin >> q;
-    for (int i = 0; i < q; i++) {
-        int o;
-        cin >> o;
-        if (o == 1) {
-            int k, v;
-            cin >> k >> v;
-            a[k - 1] = v;
-        } else {
-            int l, r;
-            cin >> l >> r;
-            cout << query(n, a, l - 1, r - 1) << "\n";
-        }
+    int min = a[0], max = 0;
+    for (int i = 0; i < n; i++) {
+        min = ::min(min, a[i]);
+        max = ::max(max, a[i] - min);
     }
+    cout << max << "\n";
 }
