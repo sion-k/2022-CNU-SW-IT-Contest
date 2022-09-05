1fbbda794bedc0b76e562daa02b50f2f56100b93
sion-k <ygonepiece@naver.com>
2022-09-06 01:09:09 +0900

    fix: different generator optimization

--- problem/prime_game/generator/different.cpp
+++ problem/prime_game/generator/different.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 // 범위가 서로 겹치지 않고 한 쪽 범위가 나머지 한 쪽 범위보다 소수의 개수가 1개 많은 데이터
-const int RANGE_MIN = 2, RANGE_MAX = 400;
+const int RANGE_MIN = 2, RANGE_MAX = 1'000;
 
 // 2부터 1000까지 소수의 개수
 const int PRIME_COUNT = 168;
@@ -21,52 +21,35 @@ bool is_prime(int x) {
     return true;
 }
 
-// 두 구간이 서로 겹치는지 확인
-bool overlaps(pair<int, int> u, pair<int, int> v) {
-    if (u.first <= v.first && v.second <= u.second) return true;
-    if (u.second < v.first || v.second < u.first) return false;
-    return true;
+// p[i], p[j] 는 양쪽 끝이 소수인 구간
+// 적당히 포함된 소수의 개수가 변하지 않도록 늘린다.
+pair<int, int> spread(vector<int> &p, int i, int j) {
+    pair<int, int> ret;
+    ret.first = rnd.next(i == 0 ? 1 : p[i - 1] + 1, p[i]);
+    ret.second = rnd.next(p[j], j == PRIME_COUNT - 1 ? p[j] : p[j + 1] - 1);
+    return ret;
 }
 
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
     
-    // 소수 계량 누적 합
-    vector<int> p(1001);
+    // 소수
+    vector<int> p;
     for (int i = 2; i <= 1000; i++) {
-        p[i] += p[i - 1];
         if (is_prime(i)) {
-            p[i]++;
+            p.push_back(i);
         }
     }
 
-    // 소수의 개수 별로 구간을 모두 저장
-    vector<vector<pair<int, int>>> range(PRIME_COUNT + 1);
-    for (int i = RANGE_MIN; i <= RANGE_MAX; i++) {
-        for (int j = i; j <= RANGE_MAX; j++) {
-            int cnt = p[j] - p[i - 1];
-            range[cnt].emplace_back(i, j);
-        }
-    }
+    // length, length + 1 길이의 구간을 두 개 고른다
+    int length = rnd.next(0, PRIME_COUNT - 2);
+    int a = rnd.next(0, PRIME_COUNT - length - 1);
+    int b = a + length;
+    int c = rnd.next(0, PRIME_COUNT - (length + 1) - 1);
+    int d = c + length + 1;
 
-    // (half, half + 1)개씩 소수가 포함되게 한다
-    int half = rnd.next(0, PRIME_COUNT / 2 - 1);
-
-    // 서로 겹치지 않는 구간 쌍을 모두 가져온다.
-    vector<tuple<int, int, int, int>> cand;
-    for (int i = 0; i < SIZE(range[half]); i++) {
-        for (int j = i; j < SIZE(range[half + 1]); j++) {
-            if (!overlaps(range[half][i], range[half + 1][j])) {
-                auto [a, b] = range[half][i];
-                auto [c, d] = range[half + 1][j];
-                cand.emplace_back(a, b, c, d);
-            }
-        }
-    }
-
-    auto [a, b, c, d] = rnd.any(cand);
-    pair<int, int> yt({ a, b });
-    pair<int, int> yj({ c, d });
+    pair<int, int> yt = spread(p, a, b);
+    pair<int, int> yj = spread(p, c, d);
 
     if (rnd.next(0, 1)) {
         swap(yt, yj);
