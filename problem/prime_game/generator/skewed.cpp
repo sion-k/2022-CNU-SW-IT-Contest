2a41d01829fa6499e973323ae294768adb3a911f
sion-k <ygonepiece@naver.com>
2022-09-06 01:21:07 +0900

    fix: skewed generator

--- problem/prime_game/generator/skewed.cpp
+++ problem/prime_game/generator/skewed.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-const int LEFT_MIN = 2, LEFT_MAX = 10;
-const int RIGHT_MIN = 990, RIGHT_MAX = 1'000;
+const int LEFT_MIN = 2, LEFT_MAX = 1'000;
+const int RIGHT_MIN = 2, RIGHT_MAX = 1'000;
 
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
     
-    int a = rnd.next(LEFT_MIN, LEFT_MAX);
-    int b = rnd.next(a, LEFT_MAX);
-    int c = rnd.next(RIGHT_MIN, RIGHT_MAX);
-    int d = rnd.next(c, RIGHT_MAX);
+    int a = rnd.next(0, 1) ? LEFT_MIN : LEFT_MAX;
+    int b = max(a, rnd.next(0, 1) ? LEFT_MIN : LEFT_MAX);
+    int c = rnd.next(0, 1) ? RIGHT_MIN : RIGHT_MAX;
+    int d = max(c, rnd.next(0, 1) ? RIGHT_MIN : RIGHT_MAX);
 
     pair<int, int> yt(a, b);
     pair<int, int> yj(c, d);
