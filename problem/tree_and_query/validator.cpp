56f3714e13cda5ac6d7c510ba6d22e5d9addb6db
sion-k <axiom0510@gmail.com>
2022-09-10 19:03:00 +0900

    chore: add new lines

--- problem/tree_and_query/validator.cpp
+++ problem/tree_and_query/validator.cpp
@@ -7,21 +7,30 @@ const int N_MIN = 1, N_MAX = 100'000;
 
 int main(int argc, char* argv[]) {
     registerValidation(argc, argv);
+
     int n = inf.readInt(N_MIN, N_MAX, "n");
     inf.readEoln();
+    
     vector<int> a = inf.readInts(n, 0, 1, "a");
     inf.readEoln();
+    
     for (int i = 0; i < n - 1; i++) {
         int u = inf.readInt(N_MIN, N_MAX, "u");
         inf.readSpace();
+
         int v = inf.readInt(N_MIN, N_MAX, "v");
         inf.readEoln();
+
+        ensuref(u != v, "The edge (%d %d) must connect different nodes.", u, v);
     }
+
     int q = inf.readInt(N_MIN, n, "q");
     inf.readEoln();
+
     for (int i = 0; i < q; i++) {
         int u = inf.readInt(N_MIN, N_MAX, "u");
         inf.readEoln();
     }
+
     inf.readEof();
 }
