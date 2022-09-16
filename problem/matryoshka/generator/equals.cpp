2ed11cb21e654408f7ddb4d7b6d015b1692886c4
sion-k <ygonepiece@naver.com>
2022-09-16 17:33:24 +0900

    fix: 문제 제한 변경

---
+++ problem/matryoshka/generator/equals.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+// 모든 수가 같은 데이터
+const int N_MIN = 1, N_MAX = 1'000;
+const int AI_MIN = 1, AI_MAX = 1e9;
+
+int main(int argc, char* argv[]) {
+    registerGen(argc, argv, 1);
+
+    int n = rnd.next(N_MIN, N_MAX);
+    cout << n << "\n";
+
+    int ai = rnd.next(AI_MIN, AI_MAX);
+
+    for (int i = 0; i < n; i++) {
+        cout << ai << " \n"[i == n - 1];
+    }
+}
